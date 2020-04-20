f3f0c7e957d24b18457199c34dfdf0c9177df323
arif sunarko <63927132+arifsunarko4@users.noreply.github.com>
2020-04-20 14:24:28 +0700

    Add files via upload

---
+++ lazy load adsense anti block killer.js
@@ -0,0 +1,3 @@
+ // Lazy Load AdSense var lazyadsense=!1;
+ window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyadsense||0!=document.body.scrollTop&&!1===lazyadsense)&&(!function(){var e=document.createElement("script");
+ e.type="text/javascript",e.async=!0,e.src="https://github.com/arifsunarko4/Andsense-Anti-Block/blob/master/Adsense%20Anti%20Adblock.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyadsense=!0)},!0); 
